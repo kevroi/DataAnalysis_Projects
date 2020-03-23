52203e88247a4a21ed5e10ae3daf6eb75f884eba
kevroi <kevinroice@outlook.com>
2020-03-23 15:57:56 +0000

    Cleaned the longitude and latitude columns into floats Managed to create the scatter plot, but it only works with seaborn 0.9.0, not 0.10.0 Attached the first regression plot result Need to analyse: 1)the two thick bands of occurrences between approx. -75 to -60 and +60 to +75 latitudes (approximately half of the two frigid zones) 2)if there are any distributions in the area between the two thick bands

--- Solar Eclipse/SolEcCalc.py
+++ Solar Eclipse/SolEcCalc.py
@@ -1,12 +1,32 @@
 import pandas as pd
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
+import seaborn as sns
 from sklearn.linear_model import LinearRegression
 
+path = "5MCSEcatalog.csv"  # load data
+df = pd.read_csv(path)  # store in dataframe
 
-path = "5MCSEcatalog.csv" #load data
-df = pd.read_csv(path) #store in dataframe
-#print(df.head())
 
-lm = LinearRegression() #create linear regression object
-# X = df[] need to find a way to store juist the numeric value of lat in here, and use N?S to indicate positive or negative
+def clean(x, d):  # function to convert longitude and latitude into right form, 2nd parameter tells which dir is +ve
+    df['i'] = np.where(x.str[-1] == d, 1, -1).astype(dtype=float,
+                                                     copy=True)  # like doing an if-else statement on a column, astype stores as float
+    x = x.str[:-1].astype(dtype=float, copy=True)
+    x = x * df['i']
+    return x
+
+
+df['Lat.'] = clean(df['Lat.'], 'N')
+df['Long.'] = clean(df['Long.'], 'E')
+
+lm = LinearRegression()  # create linear regression object
+# X = df['Long.']
+# Y = df['Lat.']
+
+
+# to first see how the data looks on a scatter plot
+width = 10
+height = 10
+plt.figure(figsize=(width, height))
+sns.regplot(x='Long.', y='Lat.', data=df)  # this only works with seaborn 0.9
+plt.show()
